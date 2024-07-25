73d3af75d11728349f3bac016bad962a70a4b915
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-07-25 12:27:55 +0300

    Added GetReservation

---
+++ Services/ResAPI/Reservation.API.Infrastructure/IReservationService.cs
@@ -0,0 +1,13 @@
+﻿using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Infrastructure;
+
+public interface IReservationService
+{
+    ReservationDTO GetResByBkgNumber(int bkgNumber);
+}
---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+
+        public ResController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpGet("id")]
+        public ReservationDTO Get(int id)
+        {
+           return _reservationService.GetResByBkgNumber(id);
+        }
+    }
+}
